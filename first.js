61d9bc2977833e3f62b1b3be0214e58c3d108d07
Elia PIZARRO SEGURA <elia.pizarrosegura@amaris.com>
2022-10-09 18:27:41 +0200

    Initial commit

---
+++ first.js
@@ -0,0 +1,13 @@
+console.log("Hey")
+let nombre = "Eli"
+console.log("tu nombre es " +nombre)
+
+let a = 20 
+let b = "elia"
+let suma = a + b
+console.log(suma)
+
+//Objetos
+let obj1 = {nombre:'Rodrigo',ap:'Perez', edad: 17}
+
+console.log(obj1.edad)
